5f6ef33d271745b31f301bf520e253d829df35fd
walle9711 <jorgev537@gmail.com>
2019-05-13 00:25:52 -0500

    modificacion a plantilla

--- Templates/plantillaur.dwt.php
+++ Templates/plantillaur.dwt.php
@@ -12,7 +12,7 @@
 
 <body id="cuerpo">
 	<div id="principal">
-		<div id="cabecera">Colocar aquí el contenido para  id "cabeceraa"</div>  		
+		<div id="cabecera"><img src="../imagenes/logo1.png" width="250" height="100" alt=""/></div>  		
   		<div id="contenido">
 			<div id="menu">Colocar aquí el contenido para  id "menu"</div>
 			Colocar aquí el contenido para  id "contenido"
