71e4d7ebd62af94071e6018b038d636672f6fb34
walle9711 <jorgev537@gmail.com>
2019-05-13 01:16:04 -0500

    modificacion plantilla

--- Templates/plantillaur.dwt.php
+++ Templates/plantillaur.dwt.php
@@ -12,12 +12,31 @@
 
 <body id="cuerpo">
 	<div id="principal">
-		<div id="cabecera"><img src="../imagenes/logo1.png" width="250" height="100" alt=""/></div>  		
+		<div id="cabecera"><img src="../imagenes/logo1.png" width="150" height="50" alt=""/></div>  		
   		<div id="contenido">
-			<div id="menu">Colocar aquí el contenido para  id "menu"</div>
-			Colocar aquí el contenido para  id "contenido"
+			<!--<div id="menu">Colocar aquí el contenido para  id "menu"</div> -->
+			<!-- TemplateBeginEditable name="contenidoeditable" -->contenidoeditable<!-- TemplateEndEditable -->
 		</div>
-  		<div id="pie">Colocar aquí el contenido para  id "pie"</div>
+  		<div id="pie" >
+			<div align="center" class="pieorden">
+				<div class="pie1"><h2>Conócenos</h2>
+					<p>Trabajar en Amazon</p>
+					<p>Información corporativa</p>
+					<p>Departamento de prensa</p>
+				</div>
+				<div class="pie1"><h2>Podemos ayudarte</h2>
+					<p>Devolver o reemplazar productos</p>
+					<p>Gestionar contenido y dispositivos</p>
+					<p>Ayuda</p>
+				</div>
+				<div class="pie1"><h2>Métodos de pago</h2>
+				
+					<p>Tarjetas de crédito y débito</p>
+					<p>Tarjetas de regalo</p>
+					<p>Meses sin intereses</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
