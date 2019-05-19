30ccd05348b364a56f8202fbfd2d722b01399a6f
Eduardoglzf <eduardogonfranco@gmail.com>
2019-05-19 15:00:31 -0500

    Se agrego

---
+++ index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/plantillauser.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Index</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+		  <link rel="stylesheet" type="text/css" href="estilos/slider_style.css"> <!--estilos slider-->
+		  		  	  
+		  <meta name="viewport" content="width = divice-width, 
+         user-scalable=no, initial-scale=1, maximum-scale=1,
+         minima-scale=1">  <!--responsive-->
+         
+         
+<!-- InstanceEndEditable -->
+
+<link rel="stylesheet" type="text/css" href="estilos/estilos.css" />  
+
+ <meta name="viewport" content="width = divice-width,
+         user-scalable=no, initial-scale=1, maximum-scale=1,
+         minima-scale=1">
+</head>
+				
+  		
+
+<body id="cuerpo">
+	
+	<div id="principal">	
+		<div id="cabecera">		
+			<div id="logo"><a href="#"><img src="imagenes/compuTec.png" width="90" height="80" alt="logoCompuTec"/></a></div> 
+			
+				<div id="menuHorizontal">
+					<ul class="menuH">
+						<li class="menuH"><a href="#">Home</a></li>
+						<li class="menuH"><a href="#">Telefonía</a></li>
+						<li class="menuH"><a href="#">Computo</a></li>
+						<li class="menuH"><a href="#">Ofertas</a></li>
+					</ul>			
+				</div>
+						
+			<div id="login"><a href="#"><img src="imagenes/loginPng.png" width="40" height="40" alt="imagen usuario"/></a>
+			<p>Iniciar sesion</p></div>					
+		
+		 </div>					 		
+			 		 		 		
+  		<div id="contenido">
+  		
+			<!--<div id="menu">Colocar aquí el contenido para  id "menu"</div> -->
+			<!-- InstanceBeginEditable name="contenidoeditable" --> 
+			
+			<div class="slider">
+          <ul>
+            <li><a href="slider0.html"><img src="imagenes/slider2.jpg" alt""></a></li>
+            <li><img src="imagenes/slider3.jpg" alt""></li>
+            <li><img src="imagenes/slider4.jpg" alt""></li>
+            <li><img src="imagenes/slider3.jpg" alt""></li>
+          </ul>
+			</div>
+			
+			
+			
+			
+			<!-- InstanceEndEditable -->
+		</div><br>
+		
+		
+		<div id="pie" >
+			<div align="center" class="pieorden">
+				<div class="pie1"><h2>Conócenos</h2>
+					<p>Trabajar en Amazon</p>
+					<p>Información corporativa</p>
+					<p>Departamento de prensa</p>
+				</div>
+				<div class="pie1"><h2>Podemos ayudarte</h2>
+					<p>Devolver o reemplazar productos</p>
+					<p>Gestionar contenido y dispositivos</p>
+					<p>Ayuda</p>
+				</div>
+				<div class="pie1"><h2>Métodos de pago</h2>
+				
+					<p>Tarjetas de crédito y débito</p>
+					<p>Tarjetas de regalo</p>
+					<p>Meses sin intereses</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+<!-- InstanceEnd --></html>
