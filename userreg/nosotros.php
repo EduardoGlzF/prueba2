a2435240a1d5e71a0a44341b903c92c9f7ca0cbe
Eduardoglzf <eduardogonfranco@gmail.com>
2019-05-21 11:57:34 -0500

    Arreglando fallo alv

---
+++ userreg/nosotros.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/plantillauser.dwt.php" codeOutsideHTMLIsLocked="false" --><head>
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Acerca de nosotros</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+	<link rel="stylesheet" type="text/css" href="../estilos/nosotros_style.css" />  
+<!-- InstanceEndEditable -->
+
+<link rel="stylesheet" type="text/css" href="../estilos/estilos.css" />  
+
+ <meta name="viewport" content="width = divice-width,
+         user-scalable=no, initial-scale=1, maximum-scale=1,
+         minima-scale=1">
+</head>
+				
+  		
+
+<body id="cuerpo">
+	
+	<div id="principal">	
+		<div id="cabecera">		
+			<div id="logo"><a href="#"><img src="../imagenes/compuTec.png" width="90" height="80" alt="logoCompuTec"/></a></div> 
+			
+				<div id="menuHorizontal">
+					<ul class="menuH">
+						<li class="menuH"><a href="#">Home</a></li>
+						<li class="menuH"><a href="#">Telefonía</a></li>
+						<li class="menuH"><a href="#">Computo</a></li>
+						<li class="menuH"><a href="#">Ofertas</a></li>
+					</ul>			
+				</div>
+						
+			<div id="login"><a href="#"><img src="../imagenes/loginPng.png" width="40" height="40" alt="imagen usuario"/></a>
+			<p>Iniciar sesion</p></div>					
+		
+		 </div>					 		
+			 		 		 		
+  		<div id="contenido">
+  		
+			<!--<div id="menu">Colocar aquí el contenido para  id "menu"</div> -->
+			<!-- InstanceBeginEditable name="contenidoeditable" -->
+			
+			<div id="nosotrosTxt">
+				
+				<h1>¡Bienvenidos a CompuTec! </h1>
+				<h2>¿Cómo te podemos ayudar?</h2>
+				<br>
+
+	<h2 class="h2nosotros">Nuestra Misión</h2>
+	<bloquote class="nosotrosBq">"Impulsamos el potencial humano y ayudamos a las familias mexicanas a mejorar y empoderar sus vidas a través de la tecnología".</bloquote>
+	
+
+	<h2 class="h2nosotros">Nuestros Valores</h2>	
+<bloquote class="nosotrosBq">	
+Aprender de los retos y de los cambios.
+Mostrar respeto, humildad e integridad.
+Liberar el potencial de nuestra gente.
+Disfrutar mientras somos los mejores.
+</bloquote>
+
+
+
+	<h2 class="h2nosotros">Nuestra Filosofía</h2>	
+<bluquote class="nosotrosBq">Creemos en satisfacer las necesidades únicas de los clientes, de principio a fin. Creemos en motivar a cada empleado para que contribuya con sus ideas y experiencias en el servicio a clientes. Nos esforzamos en tratar a los clientes como únicos y en honrar sus diferencias como grupos e individuos.</bluquote>
+
+<h2 class="h2nosotros">	Nuestra Esencia</h2>
+			
+		<bloquote class="nosotrosBq">
+		Una creencia fundamental en comprender y aprovechar las fortalezas únicas del individuo, es proveer una experiencia extraordinaria a los clientes y empleados.
+
+		Diversidad e Inclusión
+		Somos un empleador comprometido con la No Discriminación dentro de nuestros espacios de trabajo.
+
+		CompuTec no discrimina por motivos de edad, sexo, raza, color, credo, etnia, orientación sexual, identidad de género, nacionalidad, discapacidad, estado civil, religión, o cualquier otra forma de discriminación que esté prohibida por la legislación local.
+		Reconocemos que todas éstas características son importantes para formar una fuerza laboral diversa.
+				
+	
+		</bloquote>
+		
+		
+</div>						
+			
+		  <!-- InstanceEndEditable -->
+		</div><br>
+		
+		
+		<div id="pie" >
+			<div align="center" class="pieorden">
+				<div class="pie1"><h2>Conócenos</h2>
+					<p>Trabajar en Amazon</p>
+					<p>Información corporativa</p>
+					<p>Departamento de prensa</p>
+				</div>
+				<div class="pie1"><h2>Podemos ayudarte</h2>
+					<p>Devolver o reemplazar productos</p>
+					<p>Gestionar contenido y dispositivos</p>
+					<p>Ayuda</p>
+				</div>
+				<div class="pie1"><h2>Métodos de pago</h2>
+				
+					<p>Tarjetas de crédito y débito</p>
+					<p>Tarjetas de regalo</p>
+					<p>Meses sin intereses</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+<!-- InstanceEnd --></html>
