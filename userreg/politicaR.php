98d463a71f181f1e30b04e45263315f91d38c4e8
valeecavazos97 <valeecavazos97@gmail.com>
2019-05-23 08:59:17 -0500

    creacion de politicas

---
+++ userreg/politicaR.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/plantillaureg.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>CompuTec</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+
+<link rel="stylesheet" type="text/css" href="../estilos/estilos.css" />  
+
+ <meta name="viewport" content="width = divice-width,
+         user-scalable=no, initial-scale=1, maximum-scale=1,
+         minima-scale=1">
+</head>
+				
+  		
+
+<body id="cuerpo">
+	
+	<div id="principal">	
+		<div id="cabecera">		
+			<div id="logo"><a href="#"><img src="../imagenes/compuTec.png" width="90" height="80" alt="logoCompuTec"/></a></div> 
+			
+				<div id="menuHorizontal">
+					<ul class="menuH">
+						<li class="menuH"><a href="#">Inicio</a></li>
+						<li class="menuH"><a href="#">Telefonía</a></li>
+						<li class="menuH"><a href="#">Cómputo</a></li>
+						<li class="menuH"><a href="#">Ofertas</a></li>
+					</ul>			
+				</div>
+						
+							
+		
+		 </div>					 		
+			 		 		 		
+  		<div id="contenido">
+  		
+			<!--<div id="menu">Colocar aquí el contenido para  id "menu"</div> -->
+			<!-- InstanceBeginEditable name="contenidoeditable" -->contenidoeditable<!-- InstanceEndEditable -->
+		</div><br>
+		
+		
+		<div id="pie" >
+			<div align="center" class="pieorden">
+				<div class="pie1"><a href="../nosotrosU.php"><h2>Nosotros</h2></a> 
+					<p>Nuestros valores</p>
+					<p>Nuestra misión</p>
+					<p>Nuestra visión</p>
+				</div>
+				<div class="pie1"><h2>Nuestras Políticas</h2>
+					<p>Seguridad </p>
+					<p>Calidad </p>
+					<p>Devoluciones</p>
+				</div>
+				<div class="pie1"><h2>Contáctanos</h2>
+					<p>Información por correo</p>
+					<p>Servicio de Ayuda</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+<!-- InstanceEnd --></html>
