c8c1b84d2f45aeb6cf4ad389d7287a15d9922e46
walle9711 <jorgev537@gmail.com>
2019-05-23 22:48:07 -0500

    carrito

--- userreg/carrito_lista.php
+++ userreg/carrito_lista.php
@@ -1,3 +1,33 @@
+<?php require_once('../conexion/conexionbd.php'); ?>
+<?php 
+include("../funciones.php")
+?>
+<?php 
+
+if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
+  //to fully log out a visitor we need to clear the session varialbles
+  $_SESSION['MM_Username'] = NULL;
+  $_SESSION['MM_UserGroup'] = NULL;
+  $_SESSION['PrevUrl'] = NULL;
+  unset($_SESSION['MM_Username']);
+  unset($_SESSION['MM_UserGroup']);
+  unset($_SESSION['PrevUrl']);
+}
+
+?>
+<?php
+
+$varCarrito_datoscarrito = "0";
+if (isset($_SESSION["MM_IdUsuario"])) {
+  $varCarrito_datoscarrito = $_SESSION["MM_IdUsuario"];
+}
+mysqli_select_db($conexionbd,$database_conexionbd );
+$iduser=$varCarrito_datoscarrito;
+$query_datoscarrito = "SELECT * FROM tblcarrito  WHERE tblcarrito.idUsuario  = '$iduser' ";
+$datoscarrito = mysqli_query($conexionbd,$query_datoscarrito) or die(mysqli_error());
+$row_datoscarrito = mysqli_fetch_assoc($datoscarrito);
+$totalRows_datoscarrito = mysqli_num_rows($datoscarrito);
+?>
 <!doctype html>
 <html><!-- InstanceBegin template="/Templates/plantillaureg.dwt.php" codeOutsideHTMLIsLocked="false" -->
 <head>
@@ -39,7 +69,36 @@
   		<div id="contenido">
   		
 			<!--<div id="menu">Colocar aquí el contenido para  id "menu"</div> -->
-			<!-- InstanceBeginEditable name="contenidoeditable" -->contenidoeditable<!-- InstanceEndEditable -->
+			<!-- InstanceBeginEditable name="contenidoeditable" --><div align="center">
+      <h1>Carrito de la compra</h1>
+      <table width="770" border="0">
+        <tr>
+          <th width="200" bgcolor="#FFFF00" scope="col">Nombre del Producto</th>
+          <th width="180" bgcolor="#FFFF00" scope="col">Unidades</th>
+          <th width="189" bgcolor="#FFFF00" scope="col">Precio</th>
+          <th width="123" bgcolor="#FFFF00" scope="col">Acciones</th>
+        </tr>
+        <?php $preciototal = 0;?>
+        <?php do { ?>
+        
+          <tr class="brillo">
+            <td><?php echo ObtenerNombreProducto($row_datoscarrito['idProducto']); ?></td>
+            <td><?php echo $row_datoscarrito['cantidad']; ?></td>
+            <td>$<?php echo ObtenerPrecioProducto($row_datoscarrito['idProducto']); ?></td>
+            <td><a href="../admin/cancelar_pedido.php?recordID=<?php echo $row_datoscarrito['idcarrito']; ?>">Cancelar pedido</a></td>
+          </tr>
+         
+          <?php } while ($row_datoscarrito = mysql_fetch_assoc($datoscarrito)); ?>
+          
+          <tr class="brillo">
+            <td>&nbsp;</td>
+            <td bgcolor="#FF0000">total:</td>
+            <td bgcolor="#FF0000">$ <?php echo $preciototal;?></td>
+            <td>&nbsp;</td>
+          </tr>
+      </table>
+      <p>&nbsp;</p>
+    </div><!-- InstanceEndEditable -->
 		</div><br>
 		
 		
