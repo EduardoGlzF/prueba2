de2f7c6511a8c0b631c880f33f8cbefe78ee35b7
walle9711 <jorgev537@gmail.com>
2019-05-28 08:59:07 -0500

    actualizar

---
+++ error_reg.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html><!-- InstanceBegin template="/Templates/plantillauser.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta charset="utf-8">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>CompuTec </title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+
+<link rel="stylesheet" type="text/css" href="estilos/estilos.css" />  
+
+ <meta name="viewport" content="width = divice-width,
+         user-scalable=no, initial-scale=1, maximum-scale=1,
+         minima-scale=1">
+</head>
+				  		
+
+<body id="cuerpo">
+	
+	<div id="principal">	
+		<div id="cabecera">		
+			<div id="logo"><a href="#"><img src="imagenes/compuTec.png" width="90" height="80" alt="logoCompuTec"/></a></div> 
+			
+				<div id="menuHorizontal">
+					<ul class="menuH">
+						<li class="menuH"><a href="./index.php">Inicio</a></li>
+						<li class="menuH"><a href="./telefoniauser.php">Telefonía</a></li>
+						<li class="menuH"><a href="./computouser.php">Cómputo</a></li>
+						<li class="menuH"><a href="./ofertauser.php">Ofertas</a></li>
+						
+					</ul>			
+				</div>
+						
+			<div id="login"><a href="acceder.php"><img src="imagenes/loginPng.png" width="40" height="40" alt="imagen usuario"/></a>
+			<p>Iniciar sesión</p></div>					
+		
+		 </div>					 		
+			 		 		 		
+  		<div id="contenido">
+  		
+			<!--<div id="menu">Colocar aquí el contenido para  id "menu"</div> -->
+			<!-- InstanceBeginEditable name="contenidoeditable" -->error de inico de seccion<!-- InstanceEndEditable -->
+		</div><br>
+		
+		
+		<div id="pie" >
+			<div align="center" class="pieorden">
+				<div class="pie1"><a href="nosotrosU.php"><h2>Nosotros</h2></a> 
+					<p>Nuestros valores</p>
+					<p>Nuestra misión</p>
+					<p>Nuestra visión</p>
+				</div>
+				<div class="pie1"><a href="politicasU.php"><h2>Nuestras Políticas</h2></a>
+					<p>Seguridad </p>
+					<p>Calidad </p>
+					<p>Devoluciones</p>
+				</div>
+				<div class="pie1"><a href="ayuda.php"><h2>Contáctanos</h2></a>
+					<p>Información por correo</p>
+					<p>Servicio de Ayuda</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+<!-- InstanceEnd --></html>
+
+
